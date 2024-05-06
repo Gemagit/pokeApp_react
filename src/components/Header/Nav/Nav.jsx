212174a50a4ef225517a474eb75d13ea22952307
Gemagit <gemitaphotographer@gmail.com>
2024-05-06 14:05:15 +0200

    icono bola ruta relativa

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 //import iconoBola from "/Volumes/fullstack_thebridge/curso_fullstack/REACT/react_web/pokeApp_react/src/assets/icono_bola.png";
+import iconoBola from "../../../assets/icono_bola.png";
 
 const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -15,7 +16,7 @@ const Nav = () => {
 
   return (
     <nav className="nav">
-      <label htmlFor="menu" onClick={toggleMenu}>{/* <img src={iconoBola} alt="bola" width={"50px"} /> */}</label>
+      <label htmlFor="menu" onClick={toggleMenu}><img src={iconoBola} alt="bola" width={"50px"} /></label>
       <input type="checkbox" id="menu" checked={menuOpen} readOnly />
       <ul className={menuOpen ? "menu-open" : ""} onClick={closeMenu}>
         <li><Link className="letrasnav" to='/'>Home</Link></li>
